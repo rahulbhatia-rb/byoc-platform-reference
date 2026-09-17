9a77d4335043b261c6cf6943612ac64686ec8a1d
Rahul H Bhatia <rahul_harikishan@srmuniv.edu.in>
2026-09-18 02:31:41 +0530

    Add modules/eks/outputs.tf

---
+++ modules/eks/outputs.tf
@@ -0,0 +1,10 @@
+output "cluster_name" {
+  value       = aws_eks_cluster.this.name
+  description = "EKS cluster name."
+}
+
+output "cluster_endpoint" {
+  value       = aws_eks_cluster.this.endpoint
+  description = "Private EKS API endpoint."
+  sensitive   = true
+}
