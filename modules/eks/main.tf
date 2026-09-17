9496846f868148da73f5706a7cfd2defae776c13
Rahul H Bhatia <rahul_harikishan@srmuniv.edu.in>
2026-09-18 02:31:34 +0530

    Add modules/eks/main.tf

---
+++ modules/eks/main.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "cluster" {
+  name = "${var.name}-cluster"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_eks_cluster" "this" {
+  name     = var.name
+  role_arn = aws_iam_role.cluster.arn
+  version  = var.kubernetes_version
+
+  vpc_config {
+    subnet_ids              = var.private_subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = false
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.cluster]
+
+  tags = merge(var.tags, {
+    "managed-by" = "terraform"
+    "module"     = "eks"
+  })
+}
